f748e0eed2af9b5f4dc4de518b3a50018af65f7a
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-20 10:41:12 +0600

    Refactor entrypoint.sh script to support development and production modes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
-export PYTHONPATH=/app
 
-prisma generate
-uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+# Check if the RELOAD environment variable is set
+if [ "$RELOAD" = "True" ]; then
+    # Development mode
+    echo "Running in development mode with hot reloading..."
+    export PYTHONPATH=/app
+    poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+else
+    # Production mode
+    echo "Running in production mode..."
+    export PYTHONPATH=/app
+    uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+fi
