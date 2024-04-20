7337fbbb5970490763d40299314e6500103c8cba
rudrodip <official.rudrodipsarker@gmail.com>
2024-04-20 12:16:26 +0600

    Refactor entrypoint.sh script to include poetry commands for database migration and running the application in development and production modes

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,10 +5,14 @@ if [ "$RELOAD" = "True" ]; then
     # Development mode
     echo "Running in development mode with hot reloading..."
     export PYTHONPATH=/app
+    poetry shell
+    poetry run prisma db push --force-reset --accept-data-loss
     poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
 else
     # Production mode
     echo "Running in production mode..."
     export PYTHONPATH=/app
-    uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
+    poetry shell
+    poetry run prisma db push --force-reset --accept-data-loss
+    poetry run uvicorn app.main:app --host 0.0.0.0 --port 8000 --workers 4
 fi
